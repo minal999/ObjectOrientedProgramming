85b4ef52c0ee36085f55a01d9dbe5a8b73241606
Minal <78600880+minal999@users.noreply.github.com>
2022-09-14 15:07:41 +0530

    Update Employee.cs

--- assingment1/Employee.cs
+++ assingment1/Employee.cs
@@ -8,26 +8,26 @@ namespace assingment1
 {
     internal class Employee
     {
-        int EmployeeID;
-        string Name;
-        int Age;
-        string Gender;
-        double Salary;
-        string Department;
+        int employeeID;
+        string name;
+        int age;
+        string gender;
+        double salary;
+        string department;
 
         public void AddDetails()
         {
-            EmployeeID = 1;
-            Name = "Rakul";
-            Age = 26;
-            Gender = "female";
-            Salary = 80778.31;
-            Department = "Manager";
+            employeeID = 1;
+            name = "Rakul";
+            age = 26;
+            gender = "female";
+            salary = 80778.31;
+            department = "Manager";
 
         }
         public string DisplayDetails()
         {
-            return $"EmployeeID= {EmployeeID}, Name= {Name}, age= {Age}, Gender= {Gender}, Salary= {Salary}, Department= {Department}";
+            return $"EmployeeID= {employeeID}, Name= {name}, age= {age}, Gender= {gender}, Salary= {salary}, Department= {department}";
         }
 
     }
