ac97f8c0a3a8f10bdffc89c8ca3988be9abb49b4
minal999 <minughadge555@gmail.com>
2022-09-15 20:24:29 +0530

    commit

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -11,12 +11,7 @@ namespace HomeWork2
     {
         static void Main(string[] args)
         {
-            student student1 = new student();
-            student1.studentID = 1;
-            student1.name = "Kajal";
-            student1.sub1 = 90;
-            student1.sub2 = 88;
-            student1.sub3 = 80;
+            student student1= new student {studentID=1,name="kajal",sub1=88,sub2=70,sub3=97};
             student1.Result();
             Console.WriteLine(student1);
             //Console.WriteLine($"Student Details:\n Student ID:{student1.StudentID}\nStudent Name:{student1.Name}\n Student Percentage:{student1.percentage}");
